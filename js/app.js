5b511b04ddec901e129bebcb6239feb98645e991
Jeffrey <126023082+crjeffrey007@users.noreply.github.com>
2025-10-07 17:57:16 -0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// Importar SDKs desde Firebase CDN (módulos ES6)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-storage.js";
+
+// Tu configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBKdQaX27zlOWU0xwCRJPscaQR5FiqjD80",
+  authDomain: "inmobiliaria-cr.firebaseapp.com",
+  projectId: "inmobiliaria-cr",
+  storageBucket: "inmobiliaria-cr.appspot.com",
+  messagingSenderId: "594252224879",
+  appId: "1:594252224879:web:6321a05511f67e2d13309a",
+  measurementId: "G-21HRE9SEVG"
+};
+
+// Inicializar Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
