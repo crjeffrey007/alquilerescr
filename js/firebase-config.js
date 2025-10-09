d1818d93e95709d30756766277b9d421de1d049c
Jeffrey <126023082+crjeffrey007@users.noreply.github.com>
2025-10-09 19:10:26 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,20 +1,24 @@
-// js/firebase-config.js - REEMPLAZA los valores con los tuyos desde Firebase Console
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-storage.js";
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider, GithubAuthProvider } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-storage.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "", // <<-- PON TU API KEY AQUI
+  apiKey: "AIzaSyBKdQaX27zlOWU0xwCRJPscaQR5FiqjD80",
   authDomain: "inmobiliaria-cr.firebaseapp.com",
   projectId: "inmobiliaria-cr",
-  storageBucket: "inmobiliaria-cr.appspot.com",
+  storageBucket: "inmobiliaria-cr.firebasestorage.app",
   messagingSenderId: "594252224879",
   appId: "1:594252224879:web:6321a05511f67e2d13309a",
-  measurementId: ""
+  measurementId: "G-21HRE9SEVG"
 };
 
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 export const storage = getStorage(app);
+export const googleProvider = new GoogleAuthProvider();
+export const facebookProvider = new FacebookAuthProvider();
+export const githubProvider = new GithubAuthProvider();
