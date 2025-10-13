2f8a162bb3ef8e62a3bf87553171632a29e31318
Jeffrey <126023082+crjeffrey007@users.noreply.github.com>
2025-10-13 16:21:42 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -4,13 +4,15 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebas
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider, GithubAuthProvider } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-storage.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "REPLACE_API_KEY",
-  authDomain: "REPLACE_PROJECT.firebaseapp.com",
-  projectId: "REPLACE_PROJECT_ID",
-  storageBucket: "REPLACE_PROJECT.appspot.com",
-  messagingSenderId: "REPLACE_MESSAGING",
-  appId: "REPLACE_APP_ID"
+  apiKey: "AIzaSyBKdQaX27zlOWU0xwCRJPscaQR5FiqjD80",
+  authDomain: "inmobiliaria-cr.firebaseapp.com",
+  projectId: "inmobiliaria-cr",
+  storageBucket: "inmobiliaria-cr.firebasestorage.app",
+  messagingSenderId: "594252224879",
+  appId: "1:594252224879:web:6321a05511f67e2d13309a",
+  measurementId: "G-21HRE9SEVG"
 };
 
 export const app = initializeApp(firebaseConfig);
