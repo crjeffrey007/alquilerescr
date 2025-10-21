0bac0c60e72426ae013a6327d9680e0f29aa97d7
Jeffrey <126023082+crjeffrey007@users.noreply.github.com>
2025-10-21 14:42:19 -0300

    Update cloudinary.js

--- js/cloudinary.js
+++ js/cloudinary.js
@@ -1,24 +1,16 @@
-// cloudinary.js
+// cloudinary-upload.js
 const cloudName = "media-anuncios";
 const uploadPreset = "alquilerescr";
 
-// Función para subir archivos a Cloudinary
-async function uploadImages(files) {
-  const uploadedUrls = [];
+export async function subirImagen(file) {
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", uploadPreset);
 
-  for (const file of files) {
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("upload_preset", uploadPreset);
-
-    const res = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
-      method: "POST",
-      body: formData
-    });
-
-    const data = await res.json();
-    uploadedUrls.push(data.secure_url);
-  }
-
-  return uploadedUrls;
+  const res = await fetch(
+    `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+    { method: "POST", body: formData }
+  );
+  const data = await res.json();
+  return data.secure_url;
 }
