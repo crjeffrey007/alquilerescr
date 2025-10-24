6efcf817bf581574d1fc36102c9f3ac8f5b957e3
Jeffrey <126023082+crjeffrey007@users.noreply.github.com>
2025-10-24 10:59:36 -0300

    Update helpers.js

--- js/helpers.js
+++ js/helpers.js
@@ -1,1 +1,4 @@
-function q(s){return document.querySelector(s);} function showMsg(el,t){el.textContent=t; setTimeout(()=>el.textContent='',4000);}
+/* js/helpers.js */
+function q(s){ return document.querySelector(s); }
+function showMsg(el, txt, t = 4000){ if(!el) return; el.textContent = txt; setTimeout(()=>el.textContent = '', t); }
+function serializeForm(form){ return Object.fromEntries(new FormData(form).entries()); }
