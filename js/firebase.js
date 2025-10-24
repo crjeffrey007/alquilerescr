77ae58d317ecc2908eb2e59cb8a6ae371df10c6c
Jeffrey <126023082+crjeffrey007@users.noreply.github.com>
2025-10-24 10:39:05 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,15 @@
-const firebaseConfig = {"apiKey": "AIzaSyBKdQaX27zlOWU0xwCRJPscaQR5FiqD80", "authDomain": "inmobiliaria-cr.firebaseapp.com", "projectId": "inmobiliaria-cr", "storageBucket": "inmobiliaria-cr.firebasestorage.app", "messagingSenderId": "594252224879", "appId": "1:594252224879:web:6321a05511f67e2d13309a", "measurementId": "G-21HRE9SEVG"};
+/* js/firebase.js */
+const firebaseConfig = {
+  apiKey: "AIzaSyBKdQaX27zlOWU0xwCRJPscaQR5FiqD80",
+  authDomain: "inmobiliaria-cr.firebaseapp.com",
+  projectId: "inmobiliaria-cr",
+  storageBucket: "inmobiliaria-cr.firebasestorage.app",
+  messagingSenderId: "594252224879",
+  appId: "1:594252224879:web:6321a05511f67e2d13309a",
+  measurementId: "G-21HRE9SEVG"
+};
 if (!firebase.apps || !firebase.apps.length) firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore(); const auth = firebase.auth(); window.db = db; window.auth = auth;
+const db = firebase.firestore();
+const auth = firebase.auth();
+window.db = db;
+window.auth = auth;
